f422c3b4295bc26c7ccae18efeb30fdb86e79f29
ejtejada <edgar_tejada0971@elcamino.edu>
2021-09-21 22:37:48 -0700

    Making a test install script with no backup option.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Copies the configs to the current home directory, but respecting files with a newer modification date than the the current configs.
+#[TODO] Save the previous configs to some backup folder, or make .backups?
+rysnc -auvz ./ $HOME
